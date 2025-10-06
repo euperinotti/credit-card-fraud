41e82b3ddc2c244f84493b7d7f5678493a8a1afd
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-10-06 07:01:51 -0300

    refactor: analise eda

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # %% [markdown]
 # # Análise Exploratória de Dados - Fraude de Cartão de Crédito
-# 
+#
 # ## 1. Carregando as bibliotecas e os dados
 
 # %%
@@ -8,19 +8,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', None)
+pd.options.display.float_format = '{:.4f}'.format
+
 sns.set_style('whitegrid')
 plt.rcParams['figure.figsize'] = (12, 7)
 
-df = pd.read_csv('creditcard-fraud.csv')
-df.head()
-
+df = pd.read_csv('creditcard.csv')
 # %% [markdown]
 # ## 2. Inspeção Inicial e Estatísticas
 
-# %%
+# %% [markdown]
+# ## Head
+df.head()
+# ## Info & Null Values
 df.info()
+df.isnull().sum()
 
 # %%
+# count: Número de entradas não nulas
+# mean: Média
+# std: Desvio padrão
+# min: Valor mínimo
+# 25%: Primeiro quartil (Q1)
+# 50%: Mediana (Q2)
+# 75%: Terceiro quartil (Q3)
+# max: Valor máximo
 df.describe()
 
 # %% [markdown]
@@ -35,6 +49,9 @@ sns.countplot(x='Class', data=df)
 plt.title('Distribuição das Classes (0: Não Fraude, 1: Fraude)')
 plt.show()
 
+sns.countplot(data=df, x='Amount', hue='Class')
+plt.title('Distribuição do Valor da Transação por Classe')
+plt.show()
 # %% [markdown]
 # ## 4. Análise de 'Time' e 'Amount'
 
@@ -46,7 +63,7 @@ plt.show()
 # %%
 sns.histplot(df['Amount'], bins=100, kde=True)
 plt.title('Distribuição do Valor da Transação')
-plt.xlim(0, 5000) # Limita o eixo X para melhor visualização
+plt.xlim(0, 5000)
 plt.show()
 
 # %% [markdown]
